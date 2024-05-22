48a723b3bf476a4b7b50bf2c26a1796e749afb28
naufalmuhammadn <naufalmuhammadmail@gmail.com>
2024-05-22 13:46:25 +0700

    first commit

---
+++ src/services/getData.js
@@ -0,0 +1,10 @@
+const { Firestore } = require('@google-cloud/firestore');
+ 
+async function getData() {
+    const db = new Firestore();
+    const querySnapshot = await db.collection('prediction').get();
+    const user = querySnapshot.docs.map(doc => doc.data());
+    return user;
+}
+ 
+module.exports = getData;
